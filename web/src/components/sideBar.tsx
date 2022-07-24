1c6e99e9ac5a6785e505838a9595c47a6bc8b4c1
Tohar-Ts <toharbk@gmail.com>
2022-07-24 21:13:21 +0300

    global state init

--- web/src/components/sideBar.tsx
+++ web/src/components/sideBar.tsx
@@ -1,10 +1,46 @@
 import '../styles/SideBar.css';
+import { telAvivData } from "../jsons/tel-aviv-json";
+import { getCategoriesMap } from '../icons/icons-service';
+import { Checkbox } from 'semantic-ui-react';
+import { toggleCategory } from '../stateMachine/UpdateGlobalStates';
+import { useStateMachine } from 'little-state-machine';
 
 
 function SideBar() {
+
+  const CategoriesMap = getCategoriesMap(telAvivData);
+  const { state, actions } = useStateMachine({
+    toggleCategory
+  });  
+   
+  // actions.toggleCategory("lolo")
   return (   
-  <div className='sideBarContainer'>sdlifhewodf</div>
+  <div className='sideBarContainer'>
+
+    <h1> Choose category</h1>
+    {Object.keys(CategoriesMap).map((key) => (
+        <Checkbox  key={key} label={
+          <label className='categoryLabel'>
+          {key}
+          <img
+          className='icon'
+            src={CategoriesMap[key]}
+            alt={key}
+          />
+          
+          </label>} >
+
+        
+      
+        </Checkbox>
+      ))}
+
+    <h1> Choose age group</h1>
+
+
+  </div>
   );
+
 }
 
 export default SideBar;
--- web/src/icons/icons-service.ts
+++ web/src/icons/icons-service.ts
@@ -55,3 +55,6 @@ export function getImageUrl(category: string) {
       return foodStoreImage;
   }
 }
+
+
+
